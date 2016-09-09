2c2a2c2c27a4080304e1fefd45c99604c3de7cb4
Windyliupei <windyliupei@126.com>
2016-09-09 14:23:15 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+ 
+using System.Net.Sockets;
+using System.Net;
+
+using System.Threading.Tasks;
+
+
+namespace Service
+{
+     
+    public class Program
+    {
+     
+        public static string IpAddress = string.Empty;
+        public static int Port;
+        public static int MaxListenCount;
+        public static Socket ServiceSocket;
+        
+        public static void Main(string[] args)
+        {
+            LoadSettings();
+            
+
+            StartLisen();
+
+            Console.WriteLine("Press any key to exist");
+            Console.ReadLine();
+        }
+
+        
+        private static void LoadSettings()
+        {
+             // 支持中文编码
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            // 加载日志配置文件
+            var setttins = new ConfigurationBuilder().AddJsonFile("Config.json").Build();
+            IpAddress = setttins["AppSetting:HostName"];
+            Port = int.Parse(setttins["AppSetting:Port"]);
+            MaxListenCount = int.Parse(setttins["AppSetting:MaxConnections"]);
+        }
+
+        /// <summary>
+        /// 启动侦听
+        /// </summary>
+        /// <returns></returns>
+        public static void StartLisen()
+        {
+            AsyncIOCPServer server = new AsyncIOCPServer(IPAddress.Parse(IpAddress), Port, MaxListenCount);
+            server.Start();
+            Console.WriteLine("Server Started:{0}:{1}!",IpAddress,Port);
+            System.Console.ReadLine();  
+        }
+    }
+
+}
